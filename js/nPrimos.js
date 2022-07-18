1ac3cb651599664a670624a5639a8121e90d2fb2
jltlanga <julitolanga@gmail.com>
2022-07-18 01:21:12 -0300

    exercício nº 4

--- js/nPrimos.js
+++ js/nPrimos.js
@@ -1,1 +1,23 @@
+/*● Exercício 4:
+Escreva um algoritmo que leia uma lista de números inteiros e mostre o maior deles.*/
 "use strict";
+let qnt = 20;
+function nPrimos(qnt) {
+    let numero = new Array();
+    for (var i = 0; i < qnt; i++) {
+        if (isPrime(i)) {
+            numero.push(i);
+        }
+    }
+    return numero;
+}
+function isPrime(qnt) {
+    for (let i = 2; i < qnt; i++) {
+        if (qnt % i == 0) {
+            return false;
+        }
+        ;
+        return qnt > 1;
+    }
+}
+document.write(`Os n°s primos são: ${nPrimos(qnt)}`);
