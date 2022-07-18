73e90478b1d77b185528d04648b5f1144456ae13
jltlanga <julitolanga@gmail.com>
2022-07-17 23:39:21 -0300

    Exercícios 1 ts

---
+++ js/custoConsumFabCarros.js
@@ -0,0 +1,12 @@
+/*● Exercício 1:O custo ao consumidor de um carro novo é a soma do custo de fábrica com a percentagem do distribuidor e dos impostos (aplicados ao custo de fábrica). Supondo que a percentagem do distribuidor seja
+de 28% e os impostos de 45%, escrever um algoritmo que leia o custo de fábrica de um carro e escreva o custo ao consumidor. */
+"use strict";
+let custoDaFabrica = 5000;
+const custoDistribuidor = 0.28;
+const imposto = 0.45;
+function baseDeCalculo(custoDaFabrica, custoDistribuidor, imposto) {
+    let soma = custoDaFabrica + custoDaFabrica * custoDistribuidor + custoDaFabrica * imposto;
+    return soma;
+}
+let CustoConsumidor = baseDeCalculo(custoDaFabrica, custoDistribuidor, imposto);
+document.write(`Custo do consumidor: ${CustoConsumidor}<br/>`);
